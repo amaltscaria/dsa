1610d9840ef6c389da848688d3a80768a2713e31
Amal T Scaria <amaltscaria22@gmail.com>
2024-04-21 00:01:47 +0530

    Add some  recursion guess the output questions

--- Recursion/basics.js
+++ Recursion/basics.js
@@ -1,4 +1,18 @@
-// Recursive print your name n times;
+//Typical Structure of a Recursive function
+
+const recursion = () => {
+    //Base cases
+    //..........
+    //..........
+
+    //Recursive call for values that move toward a base case
+    //..........
+    //..........
+}
+
+console.log('*--------------*');
+
+// Recursively print your name n times;
 
 const fun = n => {
     //base condition
@@ -8,3 +22,56 @@ const fun = n => {
 }
 
 fun(5);
+
+console.log('*--------------*');
+//Guess the output
+
+const recursion2 = n => {
+    if(n===0) {
+        return;
+    }
+    console.log(n);
+    recursion2(n-1);
+    console.log(n);
+}
+recursion2(3)
+
+// 3
+// 2
+// 1 
+// 1
+// 2
+// 3
+
+// Guess the output
+
+console.log('*--------------*');
+
+function recursion3(n) {
+    if(n === 0) {
+        return ;
+    }
+        recursion3 (n-1);
+        console.log(n);
+        recursion3(n-1);
+    
+}
+
+recursion3(3);
+
+// 1
+// 2
+// 1
+// 3
+// 1
+// 2
+// 1 
+console.log('*--------------*');
+// Guess the output
+
+const recusrsion4 = n => {
+    if(n === 1)return 0;
+    return 1 + recusrsion4(Math.floor(n/2));
+}
+console.log(recusrsion4(25));
+
